1ddbacbc61b7e3ad48e2f94b6a8ebffef11c8dca
yl <2456236856@qq.com>
2024-03-10 22:03:30 +0800

    Day1学习更新

--- back-end-project/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ back-end-project/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -10,6 +10,7 @@ import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,9 +41,11 @@ public class EmployeeController {
     @PostMapping("/login")
     public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
         log.info("员工登录：{}", employeeLoginDTO);
+        // 将前端传递的密码加密为MD5密文，与后端数据库比较
+        employeeLoginDTO.setPassword(DigestUtils.md5DigestAsHex(employeeLoginDTO.getPassword().getBytes()));
 
         Employee employee = employeeService.login(employeeLoginDTO);
-
+        // TODO 11
         //登录成功后，生成jwt令牌
         Map<String, Object> claims = new HashMap<>();
         claims.put(JwtClaimsConstant.EMP_ID, employee.getId());
